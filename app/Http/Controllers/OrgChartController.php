02da26628ebf43fe404bb76abd74d8199abc28b1
Amiramor95 <muhdamirabdhamid@gmail.com>
2024-07-07 20:14:41 +0800

    readme & validation checking

--- app/Http/Controllers/OrgChartController.php
+++ app/Http/Controllers/OrgChartController.php
@@ -72,14 +72,16 @@ class OrgChartController extends Controller
         $employeeData=$processed['employeeData'];
         $invalidData=$processed['invalidData'];
       
-        if (!empty($employeeData) &&  empty($invalidData)) {
+        if (!empty($employeeData) &&  empty($invalidData)) { //To check if only got valid data in the csv 
             Employee::insert($employeeData);
            // return redirect()->back()->with('success', 'CSV file imported successfully.');
            return response()->json(['message' => 'Success','data'=>$employeeData ], 201);
-        } elseif(!empty($employeeData) && !empty($invalidData)){
+        } elseif(!empty($employeeData) && !empty($invalidData)){ // to check if got valid data and invalid data in the csv
             Employee::insert($employeeData);
            return response()->json(['message' => 'Success with Unsuccessful Data','Sucsessful Data'=>$employeeData , 'Unsuccessful Data'=>$invalidData ], 201);
-        }else{
+        }elseif(!empty($invalidData)){ // To check if only got invalid data in the csv 
+           return response()->json(['message' => 'Unsuccessful','Unsuccessful Data'=>$invalidData ], 201);
+        }else{ // To check if no data in csv
             return response()->json(['message' => 'Unsuccessful','No data found' ], 201);
         }
     
